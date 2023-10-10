46b37e7b9e865a08a9df0f7d678b9c3577ae9ca8
Johora-Juhi <zohrajuhi1012@gmail.com>
2023-10-10 11:47:18 +0600

    about-us banner

---
+++ about-us.php
@@ -0,0 +1,238 @@
+<!-- database connection  -->
+<?php
+include_once('./includes/connect.php');
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>About us - Delici</title>
+    <!-- stylesheet  -->
+    <link rel="stylesheet" href="./css/style.css">
+    <link rel="stylesheet" href="./css/global.css">
+    <link rel="stylesheet" href="./test.css">
+    <!-- <link rel="stylesheet" href="./test.css"> -->
+
+    <!-- favicon  -->
+    <link rel="shortcut icon" href="./images/favicon.png" type="image/x-icon">
+    <link rel="icon" href="./images/favicon.png" type="image/x-icon">
+    <!-- bootstrap  -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
+    <!-- fontawosome  -->
+    <!-- <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.6.1/css/font-awesome.min.css" rel="stylesheet" /> -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css" integrity="sha512-z3gLpd7yknf1YoNbCzqRKc4qyor8gaKU1qmn+CShxbuBusANI9QpRohGBreCFkKxLhei6S9CQXFEbbKuqLg0DA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <!-- font  -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Permanent+Marker&display=swap" rel="stylesheet">
+    <!-- sweetalert  -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/sweetalert2@11.7.27/dist/sweetalert2.min.css">
+    <!-- <link rel="stylesheet" href="sweetalert2.min.css"> -->
+    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11.7.27/dist/sweetalert2.all.min.js"></script>
+    <!-- <script src="sweetalert2.all.min.js"></script> -->
+    <!-- datepicker  -->
+    <link rel="stylesheet" href="//code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+    <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
+    <script>
+        $(function() {
+            $("#datepicker").datepicker();
+        });
+    </script>
+</head>
+
+<body>
+    <!-- Main Header  -->
+    <div class="header-container">
+        <div class="top-header">
+            <div class="top-header-info">
+                <ul class="top-left">
+                    <li>
+                        <i class="icon fas fa-map-marker-alt" style="padding: 0; color:white; min-width:14px"></i>
+                        Restaurant St, Delicious City, London 9578, UK
+                    </li>
+                    <li>
+                        <i class="icon far fa-clock" style="padding: 0; color:white;"></i>
+                        Daily : 8.00 am to 10.00 pm
+                    </li>
+                </ul>
+                <ul class="top-right">
+                    <li>
+                        <a href="tel:+11234567890">
+                            <i class="icon fas fa-phone" style="padding: 0; color:white;"></i>
+                            +1 123 456 7890
+                        </a>
+                    </li>
+                    <li>
+                        <a href="mailto:booking@restaurant.com">
+                            <i class="icon far fa-envelope" style="padding: 0; color:white;"></i>
+                            booking@restaurant.com
+                        </a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+        <div class="main-header">
+            <div class="logo"><a href="index.html" title="Delici - Restaurants HTML Template"><img src="./images/logo.png" alt="" title="Delici - Restaurants HTML Template"></a></div>
+            <nav class="nav-container">
+                <a class="nav-link" href="#">Home</a>
+                <a class="nav-link" href="./menu.php">Menu</a>
+                <a class="nav-link" href="./about-us.php">About Us</a>
+                <a class="nav-link" href="./our-chef.php">Our Chefs</a>
+                <a class="nav-link" href="./contact.php">Contact</a>
+                </ul>
+            </nav>
+            <div class="button-container">
+                <a href="./reservation.php" class="nav-button"> <button type="" class="theme-btn btn-style-one">
+                        <span class="btn-wrap">
+                            <span class="text-one">find a table</span>
+                            <span class="text-two">find a table</span>
+                        </span>
+                    </button>
+                </a>
+                <!-- Hidden Nav Toggler -->
+                <div class="info-toggler">
+                    <button class="info-bar-opener">
+                        <span class="hamburger">
+                            <span class="top-bun"></span>
+                            <span class="meat"></span>
+                            <span class="bottom-bun"></span>
+                        </span>
+                    </button>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- End Main Header  -->
+
+    <!--Info Back Drop-->
+    <div class="info-back-drop"></div>
+
+    <!-- Hidden Bar -->
+    <div class="info-bar">
+        <div class="cross-icon"><span class="fa-solid fa-close"></span></div>
+        <div class="logo-box"><a href="index.html" title="Delici - Restaurants HTML Template"><img src="./images/resource/sidebar-logo.png" alt="" title="Delici - Restaurants HTML Template"></a></div>
+        <div class="image-box"><img src="./images/resource/sidebar-image.jpg" alt="" title=""></div>
+
+        <nav class="nav-container">
+            <a class="nav-link" href="#">Home</a>
+            <a class="nav-link" href="./menu.php">Menu</a>
+            <a class="nav-link" href="./about-us.php">About Us</a>
+            <a class="nav-link" href="./our-chef.php">Our Chefs</a>
+            <a class="nav-link" href="./contact.php">Menu</a>
+            </ul>
+        </nav>
+
+        <p class="side-heading">Visit Us</p>
+        <p>Restaurant St, Delicious City, <br>London 9578, UK</p>
+        <p>Open: 9.30 am - 2.30pm</p>
+        <p><a href="mailto:booking@domainame.com">booking@domainame.com</a></p>
+
+        <div class="separator"><span></span></div>
+        <div class="booking-info">
+            <div class="bk-title">Booking request</div>
+            <div class="bk-no"><a href="tel:+88-123-123456">+88-123-123456</a></div>
+        </div>
+        <!-- </div> -->
+    </div>
+    <!--End Hidden Bar -->
+
+    <!-- Inner Banner Section -->
+    <section class="inner-banner">
+        <div class="image-layer" style="background-image: url(./images/background/banner-image-1.jpg);"></div>
+        <div class="auto-container">
+            <div class="inner">
+                <div class="subtitle"><span>our story</span></div>
+                <div class="pattern-image"><img src="./images/icons/separator.svg" alt="" title=""></div>
+                <h1><span>about us</span></h1>
+            </div>
+        </div>
+    </section>
+    <!--End Banner Section -->
+
+    <!-- Award Winiing Chef  -->
+
+    <!-- quote section  -->
+
+    <!-- meet our chef   -->
+    <!-- Footer section  -->
+    <div class="footer">
+        <div class="image-layer"></div>
+        <div class="top-footer">
+            <div class="info-box">
+                <div class="inner">
+                    <div class="logo"><a href="index.php" title="Delici - Restaurants HTML Template"><img src="./images/logo.png" alt="" title="Delici - Restaurants HTML Template"></a></div>
+                    <div class="info">
+                        <ul>
+                            <li>Restaurant St, Delicious City, London 9578, UK</li>
+                            <li><a href="mailto:booking@domainname.com">booking@domainname.com</a></li>
+                            <li><a href="tel:+88-123-123456">Booking Request : +88-123-123456</a></li>
+                            <li>Open : 09:00 am - 01:00 pm</li>
+                        </ul>
+                    </div>
+                    <div class="separator">
+                        <span></span><span></span><span></span>
+                    </div>
+                    <div class="subscribe">
+                        <div class="heading-three">
+                            Get News & Offers
+                        </div>
+                        <div class="text">Subscribe us &amp; Get <span>25% Off.</span></div>
+                    </div>
+                    <form action="" class="subscribe-form">
+                        <div class="input-group">
+                            <div class="input-icon">
+                                <i class="icon far fa-envelope"></i>
+                                <input name="email" type="email" class="input-field" placeholder="Your email" style="padding-left: 45px;">
+                            </div>
+                            <button type="submit" class="btn-style-one" name="subscribe" id="submit_btn">
+                                <span class="btn-wrap">
+                                    <span class="text-one">subscribe</span>
+                                    <span class="text-two">subscribe</span>
+                                </span>
+                            </button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+            <div class="nav-box">
+                <ul class="nav-container">
+                    <li class="nav-link"><a href="">Home</a></li>
+                    <li class="nav-link"><a href="">Menus</a></li>
+                    <li class="nav-link"><a href="">About Us</a></li>
+                    <li class="nav-link"><a href="">Our Chefs</a></li>
+                    <li class="nav-link"><a href="">Contact</a></li>
+                </ul>
+            </div>
+            <div class="nav-box last">
+                <ul class="nav-container">
+                    <li class="nav-link"><a href="#">facebook</a></li>
+                    <li class="nav-link"><a href="">instagram</a></li>
+                    <li class="nav-link"><a href="">twitter</a></li>
+                    <li class="nav-link"><a href="">you tube</a></li>
+                    <li class="nav-link"><a href="">google maps</a></li>
+                </ul>
+            </div>
+        </div>
+        <div class="bottom-footer">
+            <div class="copyright">© 2023 Delici Restaurent. All Rights Reserved </div>
+        </div>
+    </div>
+
+    <!-- js  -->
+    <script src="./js/custom.js"></script>
+    <!-- <script src="./test.js"></script> -->
+
+    <!-- <script src="./js/modal.js"></script> -->
+    <!-- jquery  -->
+    <!-- <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script> -->
+
+    <!-- preloader js  -->
+    <script src="./js/preloarder.js"></script>
+    <!-- bootstrap js  -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm" crossorigin="anonymous"></script>
+</body>
+
+</html>
